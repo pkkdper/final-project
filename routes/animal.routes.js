0d179f0a471eafdff485b5ff35fc95d373f5dd22
Andreia <andreiacpafonso@gmail.com>
2022-11-27 20:46:10 +0000

    updates

--- routes/animal.routes.js
+++ routes/animal.routes.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res, next) => {
 });
 
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", isAuthenticated, async (req, res, next) => {
     try {
         const { id } = req.params;
         const animal = await animal.findById
