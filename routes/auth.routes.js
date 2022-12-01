4b43ed72e1985ff296ae9897a49ed8c8596f4470
pkkdper <oliwia.giercza@gmail.com>
2022-12-01 09:57:07 -0500

    k

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
 const Animal = require("../models/Animal.model");
+const uploader = require('../middlewares/cloudinary.config.js');
 
 router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
@@ -17,6 +18,7 @@ router.post("/signup", async (req, res) => {
   const user = await User.create({ username, password: hashedPassword, email });
   res.status(201).json({ user });
 });
+
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
@@ -53,12 +55,6 @@ router.post("/login", async (req, res) => {
 });
 
 
-
-
-
-
-
-
 // router.get("/animals", async (req, res, next) => {
 //   const animals = await Animal.find();
 
@@ -69,25 +65,14 @@ router.post("/login", async (req, res) => {
 // });
 
 
-
-
-
-
-
-
 router.get("/profile/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id)
    .populate("animals houses").exec(function (err, results) {
-    // callback
     console.log(results)
     res.json(results);   
 });
-// key to populate
-// const userLikedRecipes = currentUser.likedRecipes
-   
-        
 
     // res.json(user);
 } catch (error) {
@@ -96,22 +81,80 @@ router.get("/profile/:id", async (req, res, next) => {
 });
 
 
+router.post("/profile/update/:id", 
+// uploader.single("imageUrl")
+async (req, res) => {
+  console.log("hi", req.body
+  // , req.file
+  )
+//   const newObj = {}
+//   for(const key in req.body) {
+//     console.log(req.body.key, req.body[key])
+// if (req.body[key] !== "undefined") {
+//   newObj[key]=req.body[key]
 
+// }
+  // } 
+  // newObj["picture"] = req.file.path
+  // console.log("newobj", newObj)
+  const { username, email, name, surname, picture, location, age} = req.body;
+  console.log("age", age===undefined)
+  console.log(age, req.body)
+  let newAge
+  if(age===undefined) {
+    newAge=0
+  } else {
+    newAge=Number(age)
+  }
+  
+  const { id } = req.params;
 
+newObjUser={ username, email, name, surname, picture, location, age:newAge}
+  // record to database
 
+  const user = await User.findByIdAndUpdate(id, newObjUser, { new: true });
+  console.log(user)
+  res.status(201).json({ payload: {userCopy:user} });
+});
 
+router.post("/profile/update/:id/image", uploader.single("imageUrl"), async (req, res) => {
+  console.log("hi", req.body, req.file)
+  const newObj = {}
+  for(const key in req.body) {
+    console.log(req.body.key, req.body[key])
+if (req.body[key] !== "undefined") {
+  newObj[key]=req.body[key]
 
-
-
-router.post("/profile/:id", async (req, res) => {
+}
+  } 
+  newObj["picture"] = req.file.path
+  console.log("newobj", newObj)
   const { username, email, name, surname, picture, location, age} = req.body;
   const { id } = req.params;
 
+
   // record to database
 
-  const user = await User.findByIdAndUpdate(id, { username, name, email, surname, picture, location, age }, { new: true });
-  res.status(201).json({ user });
+  const user = await User.findByIdAndUpdate(id, newObj, { new: true });
+  console.log(user)
+  res.status(201).json({ payload: {userCopy:user} });
 });
+
+
+router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
+  // the uploader.single() callback will send the file to cloudinary and get you and obj with the url in return
+  console.log('file is: ', req.file)
+  
+  if (!req.file) {
+    console.log("there was an error uploading the file")
+    next(new Error('No file uploaded!'));
+    return;
+  }
+  
+  // You will get the image url in 'req.file.path'
+  // Your code to store your url in your database should be here
+})
+
 
 router.get("/verify", isAuthenticated, (req, res) => {
   // isAuthenticated middleware and made available on `req.payload`
