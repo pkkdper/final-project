2520ff83da497ee6059af426866b989ad0bbdaec
Oliwia Gierczak <oliwia.giercza@gmail.com>
2023-01-02 17:40:42 +0100

    k

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const PORT = process.env.PORT || 5005;
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI =
+const MONGO_URI =  "mongodb+srv://oliwia1:BF8WK3UNDjy8W6n@cluster0.jbrejzs.mongodb.net/?retryWrites=true&w=majority"
+
   //"mongodb+srv://user1:NfJGQGrvEsBBFMZA@cluster07.bnum0b9.mongodb.net/test?retryWrites=true&w=majority";
   // mongodb+srv://<username>:<password>@cluster0.qigf07j.mongodb.net/?retryWrites=true&w=majority
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/final-project";
+  // process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/final-project";
 
 mongoose
   .connect(MONGO_URI)
